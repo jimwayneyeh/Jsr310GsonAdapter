2f533d21646e51409caa64861670d0b700f44fd5
Wayne Yeh <jimwayne@gmail.com>
2016-09-10 17:03:30 +0800

    Add adapter for converting LocalDate to and from epoch day.

---
+++ src/main/java/org/twgogo/jimwayne/gson/adapters/jsr311/LocalDateEpochAdapter.java
@@ -0,0 +1,29 @@
+package org.twgogo.jimwayne.gson.adapters.jsr311;
+
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+/**
+ * Serialize and de-serialize {@link LocalDate} to and from epoch day. 
+ */
+public class LocalDateEpochAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+  @Override
+  public JsonElement serialize (LocalDate date, Type typeOfSrc, JsonSerializationContext context) {
+    return new JsonPrimitive(date.toEpochDay());
+  }
+
+  @Override
+  public LocalDate deserialize (JsonElement json, Type typeOfT, JsonDeserializationContext context)
+      throws JsonParseException {
+    long epochDay = json.getAsLong();
+    return LocalDate.ofEpochDay(epochDay);
+  }
+}
---
+++ src/test/java/org/twgogo/jimwayne/gson/adapters/jsr311/TestLocalDateEpochAdapter.java
@@ -0,0 +1,79 @@
+package org.twgogo.jimwayne.gson.adapters.jsr311;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestLocalDateEpochAdapter {
+  private JsonParser parser = new JsonParser();
+  private Gson gson = new GsonBuilder()
+      .registerTypeAdapter(LocalDate.class, new LocalDateEpochAdapter())
+      .create();
+  
+  @Test
+  public void testSerialization () {
+    MyObject myObj = new MyObject();
+    
+    Assert.assertEquals(
+        LocalDate.MIN.toEpochDay(), 
+        this.parser.parse(this.gson.toJson(myObj.setDate(LocalDate.MIN)))
+            .getAsJsonObject()
+            .get("date").getAsLong());
+    Assert.assertEquals(
+        LocalDate.MAX.toEpochDay(), 
+        this.parser.parse(this.gson.toJson(myObj.setDate(LocalDate.MAX)))
+            .getAsJsonObject()
+            .get("date").getAsLong());
+    Assert.assertEquals(
+        LocalDate.now().toEpochDay(), 
+        this.parser.parse(this.gson.toJson(myObj.setDate(LocalDate.now())))
+            .getAsJsonObject()
+            .get("date").getAsLong());
+  }
+  
+  @Test
+  public void testDeserialization () {
+    MyObject myObj = null;
+    JsonObject json = new JsonObject();
+    
+    json.addProperty("date", LocalDate.MIN.toEpochDay());
+    myObj = this.gson.fromJson(json, MyObject.class);
+    Assert.assertEquals(
+        LocalDate.MIN.toEpochDay(), 
+        myObj.getDate().toEpochDay());
+    
+    json.addProperty("date", LocalDate.MAX.toEpochDay());
+    myObj = this.gson.fromJson(json, MyObject.class);
+    Assert.assertEquals(
+        LocalDate.MAX.toEpochDay(), 
+        myObj.getDate().toEpochDay());
+    
+    json.addProperty("date", LocalDate.now().toEpochDay());
+    myObj = this.gson.fromJson(json, MyObject.class);
+    Assert.assertEquals(
+        LocalDate.now().toEpochDay(), 
+        myObj.getDate().toEpochDay());
+  }
+  
+  private class MyObject {
+    private LocalDate date;
+    
+    public MyObject setDate (LocalDate date) {
+      this.date = date;
+      return this;
+    }
+    
+    public LocalDate getDate () {
+      return this.date;
+    }
+  }
+}
